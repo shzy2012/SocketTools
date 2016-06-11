7e478f11b270d5d42d5cba62f0b2bfd40f5e12a6
shzy2012 <shzy2012@gmail.com>
2016-06-11 19:58:54 +0800

    Update HTTPHelper.cs

--- HTTPHelper.cs
+++ HTTPHelper.cs
@@ -1,23 +1,40 @@
- public void HttpPost() {
-            string url = @"http://115.231.94.9:8001/Service/FriendsService.asmx/FriendList";
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoHttp
+{
+    public class HttpHelper
+    {
+        public static HttpWebResponse HttpPost(string url, string referer, string postDataStr)
+        {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = new CookieContainer();
+            //CookieContainer cookie = request.CookieContainer;//如果用不到Cookie，删去即可  
+            //以下是发送的http头，随便加，其中referer挺重要的，有些网站会根据这个来反盗链  
+            request.Referer = referer;
+            request.Accept = "text/html, application/xhtml+xml, image/jxr, */*";
+            request.Headers["Accept-Language"] = "zh-CN";
+            request.Headers["Accept-Charset"] = "GBK,utf-8;q=0.7,*;q=0.3";
+            request.UserAgent = "User-Agent:Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1";
+            request.KeepAlive = true;
+            //上面的http头看情况而定，但是下面俩必须加  
+            request.ContentType = "application/x-www-form-urlencoded";
             request.Method = "POST";
-            request.ContentType = "application/json";
-            string postDataStr = "{ userId:7,SessionKey:\"-1941260349\",childId:11 }";
             request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
-            //Cookie cookie = new Cookie("", "");
-            //request.CookieContainer = cookie;
             Stream myRequestStream = request.GetRequestStream();
             StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
             myStreamWriter.Write(postDataStr);
             myStreamWriter.Close();
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            //response.Cookies = cookie.GetCookies(response.ResponseUri);
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
+            
+            Encoding encoding = Encoding.Default;//根据网站的编码自定义  
+            byte[] postData = encoding.GetBytes(postDataStr);//postDataStr即为发送的数据，格式还是和上次说的一样  
+            request.ContentLength = postData.Length;
+            return (HttpWebResponse)request.GetResponse();
         }
+    }
+}
